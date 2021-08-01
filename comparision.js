0a7f30fe185804691421504761706c61baa34b90
parvezaman <emailtoamanullah@gmail.com>
2021-08-01 09:41:00 +0600

    conditional

--- comparision.js
+++ comparision.js
@@ -4,3 +4,5 @@
 console.log("6" == 6);
 console.log("6" === 6);
 // console.log(5 != 6);
+// && = and
+// || = or
---
+++ conditional.js
@@ -0,0 +1,8 @@
+var iPhonePrice = 70000;
+var myPocketBalance = 5;
+
+if (myPocketBalance > iPhonePrice) {
+    console.log("well! I think you can go for it!!!");
+} else {
+    console.log("nah!!! :D");
+}
