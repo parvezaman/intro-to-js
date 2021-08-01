646d6f685af6076de7b859537e6b63c49aa75650
parvezaman <emailtoamanullah@gmail.com>
2021-08-01 10:02:45 +0600

    multiple-condition

---
+++ multiple-condition.js
@@ -0,0 +1,9 @@
+var gotJob = false;
+var moneySaved = 5;
+var baperTaka = 500000;
+
+if ((gotJob == true && moneySaved> 200000) || baperTaka > 300000) {
+    console.log("cholo biya koiralai! :D")
+} else {
+    console.log("tor kopale biya nai :P !!!")
+}
