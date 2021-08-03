171a1479b32d1696690948eb76eacf2ce4d86512
parvezaman <emailtoamanullah@gmail.com>
2021-08-03 11:10:03 +0600

    object

---
+++ multiple-parameters.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(number1, number2){
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+}
+
+
+var firstNumber = 35;
+var secondNumber = 47;
+var result = addTwoNumbers(firstNumber, secondNumber);
+console.log("result value: " , result);
---
+++ object.js
@@ -0,0 +1,22 @@
+var student1 = {
+    id: 115,
+    name: "name of him",
+    marks: 85,
+    phone: 54549842856345,
+}
+
+var mobile = {
+    color: "black",
+    price: 16000, 
+    screenSize: 5,
+    storage: "16gb"
+}
+
+
+var book = {
+    title: "start with why",
+    author: "Simon Sinek",
+    price: 200,
+    pages: 246,
+    publisher: "penguin"
+}
