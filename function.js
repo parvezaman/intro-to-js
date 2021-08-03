7f852434c105d9e80fa23d4f4427bad7885e7393
parvezaman <emailtoamanullah@gmail.com>
2021-08-03 07:14:48 +0600

    function

---
+++ function.js
@@ -0,0 +1,6 @@
+function addNumber(num1, num2,) {
+    var num1 = parseFloat(prompt("input first number: "));
+    var num2 = parseFloat(prompt("input second number: "));
+    console.log("THE SUM IS: " , num1+num2);
+}
+addNumber();
